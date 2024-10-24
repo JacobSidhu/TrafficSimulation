8666d4e41d1324e4bef4b698e0cd0dcbf2525d06
JacobSidhu <jacobsidhu100@gmail.com>
2024-10-24 18:39:26 +0000

    1. Folder name models created into src, to store project models e.g classes. 2. Road.java file was created to store blueprint of the Road object 3. Class: Road was updated with basic blueprint

--- src/models/Road.java
+++ src/models/Road.java
@@ -1,5 +1,71 @@
 package models;
 
+// This file contains Road Class that represents blueprint for roads used in
+// in the traffic Simulation.
+
 public class Road {
+
+    // Class Attributes
+    private String roadName = null;
+    private int roadCapacity;
+    private int onRoadVehicles;
+    private boolean isEntryAllowed;
+    private Vehicle[] roadBuffer;
+    private String[] destinations;
+
+    // Class Constructor
+    Road(){}
+
+    // Methods
+    void addVehicleIntoRoad(){}
+    Vehicle getVehicleFromRoad(){}
+    boolean isSpaceAvailable(){}
     
+
+    // Setters
+    public void setEntryAllowed(boolean isEntryAllowed) {
+        this.isEntryAllowed = isEntryAllowed;
+    }
+
+    public void setDestinations(String[] destinations) {
+        this.destinations = destinations;
+    }
+
+    public void setOnRoadVehicles(int onRoadVehicles) {
+        this.onRoadVehicles = onRoadVehicles;
+    }
+
+    public void setRoadBuffer(Vehicle[] roadBuffer) {
+        this.roadBuffer = roadBuffer;
+    }
+
+    public void setRoadCapacity(int roadCapacity) {
+        this.roadCapacity = roadCapacity;
+    }
+    public void setRoadName(String roadName) {
+        this.roadName = roadName;
+    }
+
+    //Getters
+    public String[] getDestinations() {
+        return destinations;
+    }
+
+    public int getOnRoadVehicles() {
+        return onRoadVehicles;
+    }
+
+    public Vehicle[] getRoadBuffer() {
+        return roadBuffer;
+    }
+
+    public int getRoadCapacity() {
+        return roadCapacity;
+    }
+
+    public String getRoadName() {
+        return roadName;
+    }
+
+
 }
