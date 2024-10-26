f599075ecd9cda0c71a541c41880620d82e9d3b0
JacobSidhu <jacobsidhu100@gmail.com>
2024-10-26 15:02:42 +0000

    Ref: Removed unnecessary getters and setters as they require no logic

--- src/models/Junction.java
+++ src/models/Junction.java
@@ -3,21 +3,21 @@ package models;
 public class Junction {
 
     // Class Attributes
-    private int totalRoadLinked;
-    private int numOfEntryRoads;
-    private int numOfExitRoads;
-    private String junctionName;
-    private Road[] linkedRoads;
+    public int totalRoadLinked;
+    public int numOfEntryRoads;
+    public int numOfExitRoads;
+    public String junctionName;
+    public Road[] linkedRoads;
 
     public Junction createInstance(String junctionName, int totalRoadLinked, int numOfEntryRoads, int numOfExitRoads, Road[] roads){
         // Creating New Junction Instance
         Junction instance = new Junction();
         //Assigning values to the junction
-        instance.setJunctionName(junctionName);
-        instance.setTotalRoadLinked(totalRoadLinked);
-        instance.setNumOfEntryRoads(numOfEntryRoads);
-        instance.setNumOfExitRoads(numOfExitRoads);
-        instance.setLinkedRoads(roads);
+        instance.junctionName = junctionName;
+        instance.totalRoadLinked = totalRoadLinked;
+        instance.numOfEntryRoads = numOfEntryRoads;
+        instance.numOfExitRoads = numOfExitRoads;
+        instance.linkedRoads = roads;
         // Returning Class Instance.
         return instance; 
     }
@@ -28,39 +28,5 @@ public class Junction {
     static Road findDestinyRoad(String destination){}
     static void addVehicleIntoRoad(Road road){}
     static void log(){}
-
-    // Setters
-    public void setJunctionName(String junctionName) {
-        this.junctionName = junctionName;
-    }
-    public void setLinkedRoads(Road[] linkedRoads) {
-        this.linkedRoads = linkedRoads;
-    }
-    public void setNumOfEntryRoads(int numOfEntryRoads) {
-        this.numOfEntryRoads = numOfEntryRoads;
-    }
-    public void setNumOfExitRoads(int numOfExitRoads) {
-        this.numOfExitRoads = numOfExitRoads;
-    }
-    public void setTotalRoadLinked(int totalRoadLinked) {
-        this.totalRoadLinked = totalRoadLinked;
-    }
-
-    // Getters
-    public String getJunctionName() {
-        return junctionName;
-    }
-    public Road[] getLinkedRoads() {
-        return linkedRoads;
-    }
-    public int getNumOfEntryRoads() {
-        return numOfEntryRoads;
-    }
-    public int getNumOfExitRoads() {
-        return numOfExitRoads;
-    }
-    public int getTotalRoadLinked() {
-        return totalRoadLinked;
-    }
 
 }
